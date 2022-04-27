a948aa3c85ce11aea899ab89aeae8b5742b2f18c
Santiago Silvani <silvanis@bc.edu>
2022-04-27 18:15:45 -0400

    Initial Commit

---
+++ F1_App_Final2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  F1_App_Final2
+//
+//  Created by Santi Silvani on 4/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
